69a85c99d9c482608507e596e663efa5577677dd
evan54 <evan54@gmail.com>
2020-06-16 04:15:12 -0400

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='MySimpleGui',
+      version='1.0',
+      description='Additional GUI helpers for PySimpleGUI',
+      py_modules=['mysimplegui'],
+      author='evan54',
+      )
+
